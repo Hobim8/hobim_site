a3140a589e945598873ee3a00b8e1c489c03ab49
hobim <olaolukomo@gmail.com>
2026-09-04 23:50:04 +0100

    Add initial backend setup with database connection and requirements

---
+++ backend/app/database.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker, declarative_base 
+from dotenv import load_dotenv 
+import os 
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, connect_args={"connect_timeout": 10})
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    try:
+        with engine.connect() as connection:
+            print("successfully connnected")
+    except Exception as e:
+        print(f"failed to connect : {e}")
+
+
+
+
+
+
+
+
+
+
+
